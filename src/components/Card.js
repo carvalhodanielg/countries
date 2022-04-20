5e409ee53e1ef0a68cf0c1673bea077403e82453
carvalhodaniel <carvalhodanielg@gmail.com>
2022-04-19 23:23:44 -0300

    adding languages

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,8 @@ export const Cards = ({flags}) => {
 
 
 
+
+
              <div className="flagArea">
                    <img src={flags.flags.png} alt=""/> 
             </div>
@@ -23,6 +25,20 @@ export const Cards = ({flags}) => {
             <div className="infosArea">
                     Capital: {flags.capital} <br/> <br/>
                     Languages: 
+                    {flags.languages !== undefined &&
+
+                    Object.values(flags.languages).map((item)=>(
+                        <span> {item}, </span>
+                    ))
+                    
+
+
+                //    <span>{Object.values(flags.languages)}</span> 
+
+                    }
+
+
+
                      <br/><br/>
                     Population: {flags.population} <br/> <br/>
                     Currency:
